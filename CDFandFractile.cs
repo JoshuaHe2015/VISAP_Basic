cf6e7163b5af8ebe6640773e6939c8352df9495f
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-16 13:45:50 +0800

    add

---
+++ CDFandFractile.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDFandFractile
+{
+    class Program
+    {
+        static double BetaCDF(double x, double a, double b)
+        {
+            int m, n;
+            double I = 0, U = 0;
+            double ta = 0, tb = 0;
+            m = (int)(2 * a);
+            n = (int)(2 * b);
+            if (m % 2 == 1 && n % 2 == 1)
+            {
+                ta = 0.5;
+                tb = 0.5;
+                U = Math.Sqrt(x * (1.0 - x)) / Math.PI;
+                I = 1.0 - 2.0 / Math.PI * Math.Atan(Math.Sqrt((1.0 - x) / x));
+            }
+            else if (m % 2 == 1 && n % 2 == 0)
+            {
+                ta = 0.5;
+                tb = 0.1;
+                U = 0.5 * Math.Sqrt(x) * (1.0 - x);
+                I = Math.Sqrt(x);
+            }
+            else if (m % 2 == 0 && n % 2 == 1)
+            {
+                ta = 1;
+                tb = 0.5;
+                U = 0.5 * x * Math.Sqrt(1.0 - x);
+                I = 1.0 - Math.Sqrt(1.0 - x);
+            }
+            else if (m % 2 == 0 && n % 2 == 0)
+            {
+                ta = 1;
+                tb = 1;
+                U = x * (1.0 - x);
+                I = x;
+            }
+            while (ta < a)
+            {
+                I = I - U / ta;
+                U = (ta + tb) / ta * x * U;
+                ta++;
+            }
+            while (tb < b)
+            {
+                I = I + U / tb;
+                U = (ta + tb) / tb * (1.0 - x) * U;
+                tb++;
+            }
+            return I;
+        }
+        static double TDIST(double x, int v)
+        {
+            double t, prob;
+            t = v / (v + x * x);
+            if (x > 0)
+            {
+                prob = 1 - 0.5 * BetaCDF(t, v / 2.0, 0.5);
+            }
+            else
+            {
+                prob = 0.5 * BetaCDF(t, v / 2.0, 0.5);
+            }
+            return prob;
+        }
+        static double FCDF(double x, int m, int n)
+        {
+            double y, prob;
+            if (x <= 0)
+            {
+                return 0;
+            }
+            y = m * x / (n + m * x);
+            prob = BetaCDF(y, m / 2.0, n / 2.0);
+            return prob;
+        }
+        static double BinomialCDF(double x, double p, int n)
+        {
+            double prob = 0.0;
+            if (x < 0)
+            {
+                prob = 0.0;
+                return prob;
+            }
+            else if (x >= n)
+            {
+                prob = 1.0;
+                return prob;
+            }
+            else
+            {
+                prob = BetaCDF(1.0 - p, n - (int)x, (int)x + 1);
+                return prob;
+            }
+        }
+        static double BetaUa(double af, double a, double b)
+        {
+            int MaxTime = 500;
+            int times = 0;
+            double x1, x2, xn = 0.0;
+            double f1, f2, fn, ua;
+            double eps = 1.0e-10;
+            x1 = 0.0;
+            x2 = 1.0;
+            f1 = BetaCDF(x1, a, b) - (1.0 - af);
+            f2 = BetaCDF(x2, a, b) - (1.0 - af);
+            while (Math.Abs((x2 - x1) / 2.0) > eps)
+            {
+                xn = (x1 + x2) / 2.0;
+                fn = BetaCDF(xn, a, b) - (1.0 - af);
+                if (f1 * fn < 0)
+                {
+                    x2 = xn;
+                }
+                else if (fn * f2 < 0)
+                {
+                    x1 = xn;
+                }
+                f1 = BetaCDF(x1, a, b) - (1.0 - af);
+                f2 = BetaCDF(x2, a, b) - (1.0 - af);
+                times++;
+                if (times > MaxTime)
+                {
+                    break;
+                }
+            }
+            ua = xn;
+            return ua;
+        }
+        static double TINV(double af, int v)
+        {
+            double ua = 0.0, tbp, bf;
+            bf = 1 - af;
+            if (af <= 0.5)
+            {
+                tbp = BetaUa(1 - 2 * af, v / 2.0, 0.5);
+                ua = Math.Sqrt(v / tbp - v);
+            }
+            else if (af > 0.5)
+            {
+                tbp = BetaUa(1 - 2 * (1.0 - af), v / 2.0, 0.5);
+                ua = -Math.Sqrt(v / tbp - v);
+            }
+            return ua;
+        }
+        static double FdistUa(double af,int m,int n)        {            double ua,tbp,bf;             bf=1-af;            tbp=BetaUa(af,m/2.0,n/2.0);            ua=n*tbp/(m*(1.0-tbp));
+            return ua;
+        }
+        static double chi21(double x, int Freedom)
+        {
+            int k, n;
+            double f, h, prob;
+            k = Freedom % 2;
+            if (k == 1)
+            {
+                f = Math.Exp(-x / 2.0) / Math.Sqrt(2 * Math.PI * x);
+                h = 2.0 * GaossFx1(Math.Sqrt(x)) - 1.0;
+                n = 1;
+                while (n < Freedom)
+                {
+                    n = n + 2;
+                    f = x / (n - 2.0) * f;
+                    h = h - 2.0 * f;
+                }
+            }
+            else
+            {
+                f = Math.Exp(-x / 2.0) / 2.0;
+                h = 1.0 - Math.Exp(-x / 2.0);
+                n = 2;
+                while (n < Freedom)
+                {
+                    n = n + 2;
+                    f = x / (n - 2.0) * f;
+                    h = h - 2.0 * f;
+                }
+            }
+            prob = h;
+            return prob;
+        }
+        static double chi2(double x, int Freedom)  //卡方分布函数计算  
+        {
+            int k, n;
+            double f, h, prob;
+            k = Freedom % 2;
+            if (k == 1)
+            {
+                f = Math.Exp(-x / 2.0) / Math.Sqrt(2 * Math.PI * x);
+                h = 2.0 * GaossFx1(Math.Sqrt(x)) - 1.0;
+                n = 1;
+                while (n < Freedom)
+                {
+                    n = n + 2;
+                    f = x / (n - 2.0) * f;
+                    h = h - 2.0 * f;
+                }
+            }
+            else
+            {
+                f = Math.Exp(-x / 2.0) / 2.0;
+                h = 1.0 - Math.Exp(-x / 2.0);
+                n = 2;
+                while (n < Freedom)
+                {
+                    n = n + 2;
+                    f = x / (n - 2.0) * f;
+                    h = h - 2.0 * f;
+                }
+            }
+            prob = h;
+            return prob;
+        } 
+        static double PossionCDF(double x,double p)
+        {
+            double prob = 0.0;            prob=1.0-chi21(2*p,2*((int)x)+1);
+            return prob;        }
+        static double chi2Ua0(double af, int Freedom)
+        {
+            double ua, p, temp;
+            if (Freedom == 1)
+            {
+                p = 1.0 - (1.0 - af + 1.0) / 2.0;
+                temp = NORMSINV(p);
+                ua = temp * temp;
+            }
+            else if (Freedom == 2)
+            {
+                ua = -2.0 * Math.Log(af);
+            }
+            else
+            {
+                temp = 1.0 - 2.0 / (9.0 * Freedom) + Math.Sqrt(2.0 / (9.0 * Freedom)) * NORMSINV(af);
+                ua = Freedom * (temp * temp * temp);
+            }
+            return ua;
+        }
+        
+        static double chi2Ua(double af, int Freedom)//卡方分布的上侧分位数的计算  
+        {
+            int times;
+            int MaxTime = 500;
+            double eps = 1.0e-10;
+            double ua, x0, xn, bf;
+            bf = 1 - af;
+            x0 = chi2Ua0(af, Freedom);
+            if (Freedom == 1 || Freedom == 2)
+            {
+                ua = x0;
+            }
+            else
+            {
+                times = 1;
+                xn = x0 - (chi21(x0, Freedom) - 1 + af) / chi2Px(x0, Freedom);
+                while (Math.Abs(xn - x0) > eps)
+                {
+                    x0 = xn;
+                    xn = x0 - (chi21(x0, Freedom) - 1 + af) / chi2Px(x0, Freedom);
+                    times++;
+                    if (times > MaxTime) break;
+                }
+                ua = xn;
+            }
+            return ua;
+        }
+        static double chi2Px(double x, int Freedom)//卡方分布的密度函数  
+        {
+            double p, g;
+            if (x <= 0) return 0.0;
+            g = Gama(Freedom);
+            p = 1.0 / Math.Pow(2.0, Freedom / 2.0) / g * Math.Exp(-x / 2.0) * Math.Pow(x, Freedom / 2.0 - 1.0);
+            return p;
+        }
+        static double Gama(int n)//伽马分布函数Gama(n/2)  
+        {
+            double g;
+            int i, k;
+            k = n / 2; if (n % 2 == 1)
+            {
+                g = Math.Sqrt(Math.PI) * 0.5;
+                for (i = 1; i < k; i++)
+                    g *= (i + 0.5);
+            }
+            else
+            {
+                g = 1.0;
+                for (i = 1; i < k; i++)
+                    g *= i;
+            }
+            return g;
+        }
+        static double GaossFx1(double x)
+        {
+            double prob = 0, t, temp;
+            int i, n, symbol;
+            temp = x;
+            if (x < 0)
+                x = -x;
+            n = 28;
+            if (x >= 0 && x <= 3.0)
+            {
+                t = 0.0;
+                for (i = n; i >= 1; i--)
+                {
+                    if (i % 2 == 1) symbol = -1;
+                    else symbol = 1;
+                    t = symbol * i * x * x / (2.0 * i + 1.0 + t);
+                }
+                prob = 0.5 + GaossPx(x) * x / (1.0 + t);
+            }
+            else if (x > 3.0)
+            {
+                t = 0.0;
+                for (i = n; i >= 1; i--)
+                    t = 1.0 * i / (x + t);
+                prob = 1 - GaossPx(x) / (x + t);
+            }
+            x = temp;
+            if (x < 0)
+                prob = 1.0 - prob; return prob;
+        }
+        static double GaossFx(double x)//正态分布函数的计算  
+        {
+            double prob = 0, t, temp;
+            int i, n, symbol;
+            temp = x;
+            if (x < 0)
+                x = -x;
+            n = 28;//连分式展开的阶数  
+            if (x >= 0 && x <= 3.0)//连分式展开方法  
+            {
+                t = 0.0;
+                for (i = n; i >= 1; i--)
+                {
+                    if (i % 2 == 1) symbol = -1;
+                    else symbol = 1;
+                    t = symbol * i * x * x / (2.0 * i + 1.0 + t);
+                }
+                prob = 0.5 + GaossPx(x) * x / (1.0 + t);
+            }
+            else if (x > 3.0)
+            {
+                t = 0.0;
+                for (i = n; i >= 1; i--)
+                    t = 1.0 * i / (x + t);
+                prob = 1 - GaossPx(x) / (x + t);
+            }
+            x = temp;
+            if (x < 0)
+                prob = 1.0 - prob;
+            return prob;
+        }
+        static double GaossPx(double x)//正态分布的密度函数  
+        {
+            double f;
+            f = 1.0 / Math.Sqrt(2.0 * Math.PI) * Math.Exp(-x * x / 2.0);
+            return f;
+        }
+
+        public static double NORMSINV(double alpha)
+        {
+            if (0.5 < alpha && alpha < 1)
+            {
+                alpha = 1 - alpha;
+            }
+            double[] b = new double[11];
+            b[0] = 0.1570796288 * 10;
+            b[1] = 0.3706987906 * 0.1;
+            b[2] = -0.8364353589 * 0.001;
+            b[3] = -0.2250947176 * 0.001;
+            b[4] = 0.6841218299 * 0.00001;
+            b[5] = 0.5824238515 * 0.00001;
+            b[6] = -0.1045274970 * 0.00001;
+            b[7] = 0.8360937017 * 0.0000001;
+            b[8] = -0.3231081277 * 0.00000001;
+            b[9] = 0.3657763036 * 0.0000000001;
+            b[10] = 0.6657763036 * 0.000000000001;
+            double sum = 0;
+            double y = -Math.Log(4 * alpha * (1 - alpha));
+            for (int i = 0; i < 11; i++)
+            {
+                sum += b[i] * Math.Pow(y, i);
+            }
+            return Math.Pow(sum * y, 0.5);
+        }
+        static void Main(string[] args)
+        {
+            double x1 = 0.5,x2 = 0.7;
+            double a = 3, b = 5;
+            int v = 15;
+            Console.WriteLine("BetaCDF(a = {0},b = {1}) = {2}",a,b, BetaCDF(x1, a, b));
+            Console.WriteLine("TDIST(x = {0},v = {1}) = {2}", x2, v, TDIST(x2, v));
+            int m = 6, n = 7;
+            double x3 = 0.4;
+            Console.WriteLine("FCDF(x = {0},m = {1}, n = {2}) = {3}", x3, m, n, FCDF(x3, m, n));
+            Console.WriteLine("BinomialCDF = {0}",BinomialCDF(3,0.6,5));            Console.WriteLine("Kai^2={0}",chi2(0.7,2));
+            Console.WriteLine("PossionCDF = {0}",PossionCDF(0.3,0.7));
+            Console.WriteLine("FdistUa={0}",FdistUa(0.3,5,5));
+            Console.WriteLine("TINV={0}",TINV(0.4,3));            Console.WriteLine("chi^2Ua = {0}",chi2Ua(0.7,10));
+            Console.ReadKey();
+        }
+    }
+}
